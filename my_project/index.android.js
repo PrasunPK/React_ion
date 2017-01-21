195e8a8c334aa3021ff02c654c560bb6d0da6e2b
Prasun <prasun_1993@rediffmail.com>
2017-01-21 12:09:35 +0530

    Initial commit - basic setup

---
+++ my_project/index.android.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Alert,
+  Text,
+  Button,
+  View
+} from 'react-native';
+
+const onButtonPress = () => {
+  Alert.alert('Button has been pressed!');
+};
+
+export default class my_project extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        
+        <Text style={styles.welcome}>
+          Welcome to HELL
+        </Text>
+        <Button
+          title="Press here"
+          color="#841584"
+          onPress={onButtonPress}
+        />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('my_project', () => my_project);
